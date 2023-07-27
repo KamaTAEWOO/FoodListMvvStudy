c51f0ead80acf158a82c4e89884ebef60930af69
KamaTAEWOO <zszs2020@naver.com>
2023-07-28 08:19:59 +0900

    [fix] null 처리 변경

--- app/src/main/java/com/dk/foodlistmvvstudy/mapper/FoodMapper.kt
+++ app/src/main/java/com/dk/foodlistmvvstudy/mapper/FoodMapper.kt
@@ -5,25 +5,24 @@ import com.dk.foodlistmvvstudy.model.FoodEntity
 
 object FoodMapper {
 
-    fun FoodEntity.toRequest() = Food (
-        cnt = cnt ?: "null",
-        opendataId = OPENDATA_ID ?: "null",
-        gngCs = GNG_CS ?: "null",
-        fdCs = FD_CS ?: "null",
-        bzNm = BZ_NM ?: "null",
-        tlno = TLNO ?: "null",
-        mbzHr = MBZ_HR ?: "null",
-        seatCnt = SEAT_CNT ?: "null",
-        pkpl = PKPL ?: "null",
-        hp = HP ?: "null",
-        psbFrn = PSB_FRN ?: "null",
-        bknYn = BKN_YN ?: "null",
-        infnFcl = INFN_FCL ?: "null",
-        brftYn = BRFT_YN ?: "null",
-        dssrtYn = DSSRT_YN ?: "null",
-        mnu = MNU ?: "null",
-        smplDesc = SMPL_DESC ?: "null",
-        sbw = SBW ?: "null",
-        bus = BUS ?: "null",
-    )
+    fun FoodEntity.toRequest() = Food(
+        cnt = cnt,
+        opendataId = OPENDATA_ID,
+        gngCs = GNG_CS,
+        fdCs = FD_CS,
+        bzNm = BZ_NM,
+        tlno = TLNO,
+        mbzHr = MBZ_HR,
+        seatCnt = SEAT_CNT,
+        pkpl = PKPL,
+        hp = HP,
+        psbFrn = PSB_FRN,
+        bknYn = BKN_YN,
+        infnFcl = INFN_FCL,
+        brftYn = BRFT_YN,
+        dssrtYn = DSSRT_YN,
+        mnu = MNU,
+        smplDesc = SMPL_DESC,
+        sbw = SBW,
+        bus = BUS,)
 }
