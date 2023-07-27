da5f6cf0f90ca2118f2039bedb88c17bfe56f7b0
KamaTAEWOO <zszs2020@naver.com>
2023-07-27 18:20:50 +0900

    [feat] viewModel 사용하여 ui 적용

--- app/src/main/java/com/dk/foodlistmvvstudy/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/dk/foodlistmvvstudy/viewmodel/MainViewModel.kt
@@ -1,8 +1,24 @@
 package com.dk.foodlistmvvstudy.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dk.foodlistmvvstudy.repository.FoodRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-//@HiltViewModel
-class MainViewModel : ViewModel() {
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val foodRepository: FoodRepository
+) : ViewModel() {
+
+    val _foodListData = MutableLiveData<String>()
+    var foodListData = _foodListData
+
+    fun getFoodList() {
+        viewModelScope.launch {
+            foodListData.value = foodRepository.getFoodList().toString()
+        }
+    }
 }
