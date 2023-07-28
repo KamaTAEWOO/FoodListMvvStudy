fbc21a7e931df33d86a4d7cf28ef43efda100765
KamaTAEWOO <zszs2020@naver.com>
2023-07-28 14:53:43 +0900

    [fix] Food Class, FoodMapper 파일 삭제 후 FoodEntity로 처리

--- app/src/main/java/com/dk/foodlistmvvstudy/repository/FoodRepositoryImpl.kt
+++ app/src/main/java/com/dk/foodlistmvvstudy/repository/FoodRepositoryImpl.kt
@@ -1,6 +1,5 @@
 package com.dk.foodlistmvvstudy.repository
 
-import com.dk.foodlistmvvstudy.mapper.FoodMapper.toRequest
 import com.dk.foodlistmvvstudy.model.FoodEntity
 import com.dk.foodlistmvvstudy.service.FoodService
 import javax.inject.Inject
@@ -9,11 +8,13 @@ class FoodRepositoryImpl @Inject constructor(
     private val foodService: FoodService
 ) : FoodRepository {
 
-    override suspend fun getFoodList(): List<FoodEntity> {
-        foodService.getFoodList().data.also { list ->
-             list.map { it.toRequest() } // FoodEntity -> Food 전달만 함.!
-        }
-
-        return foodService.getFoodList().data
-    }
+    override suspend fun getFoodList(): List<FoodEntity> = foodService.getFoodList().data
 }
+
+// 1번 방법 (Food Data Class 사용)
+//    override suspend fun getFoodList(): List<FoodEntity> {
+////        foodService.getFoodList().data.also { list ->
+////             list.map { it.toRequest() } // FoodEntity -> Food 전달만 함.!
+////        }
+//        return foodService.getFoodList().data
+//    }
