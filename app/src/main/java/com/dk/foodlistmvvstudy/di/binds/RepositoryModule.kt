d3d63fe163ce32ddf82b236d73818b5e9f8cf0af
kamamerona <zszs2020@naver.com>
2023-08-03 20:33:37 +0900

    [feat] Hilt Binds를 사용하여 ViewModel에서 Repository 사용

--- app/src/main/java/com/dk/foodlistmvvstudy/di/binds/RepositoryModule.kt
+++ app/src/main/java/com/dk/foodlistmvvstudy/di/binds/RepositoryModule.kt
@@ -2,8 +2,10 @@ package com.dk.foodlistmvvstudy.di.binds
 
 import com.dk.foodlistmvvstudy.repository.FoodRepository
 import com.dk.foodlistmvvstudy.repository.FoodRepositoryImpl
+import com.dk.foodlistmvvstudy.service.FoodService
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
@@ -18,10 +20,4 @@ abstract class RepositoryModule {
         // Provides는 service로 받았지만, Binds는 Impl로 받음.
         foodRepositoryImpl: FoodRepositoryImpl
     ): FoodRepository
-
-//    @Singleton
-//    @Binds
-//    abstract fun bindsFoodRepository(
-//        foodService: FoodService
-//    ): FoodRepository = FoodRepositoryImpl(foodService)
 }
